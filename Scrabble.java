bb81673b4f569f1aea8b6d74a0d41c221e86a4a5
הילה מעברי <hila.mabari@post.runi.ac.il>
2024-12-06 13:36:45 +0200

    Done

--- Scrabble.java
+++ Scrabble.java
@@ -116,16 +116,13 @@ public class Scrabble {
 			else {
 				score += wordScore(input);
 				System.out.println(input + " earned " + wordScore(input) + " points. Score: " + score + " points");
-				System.out.println();
 				hand = MyString.remove(hand, input);
 			}
 		}
 		if (hand.length() == 0) {
 	        System.out.println("Ran out of letters. Total score: " + score + " points");
-			System.out.println();
 		} else {
 			System.out.println("End of hand. Total score: " + score + " points");
-			System.out.println();
 		}
 	}
 
